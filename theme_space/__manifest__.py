9fab106c29489a702fe37eed3e9ca458518a97c3
Sanjay Sharma <shsa@odoo.com>
2024-08-05 18:11:05 +0530

    [ADD](theme): customized snippets and images for theme previews
    - Added custom snippets: s_cover, s_text_image,s_title, s_features,
    s_image_text, s_product_catalog
    - Included new images for theme previews and static description
    - Updated manifest to include new views and assets

--- theme_space/__manifest__.py
+++ theme_space/__manifest__.py
@@ -8,9 +8,27 @@
     'data': [
         'data/ir_asset.xml',
         'data/generate_primary_templates.xml',
+        'views/image_library.xml',
+
+        'views/snippets/s_cover.xml',
+        'views/snippets/s_text_image.xml',
+        'views/snippets/s_title.xml',
+        'views/snippets/s_features.xml',
+        'views/snippets/s_image_text.xml',
+        'views/snippets/s_product_catalog.xml',
         # 'views/footer.xml',
         # 'views/header.xml',
     ],
+    'images': [
+        'static/description/space_description.jpg',
+        'static/description/space_screenshot.jpg',
+    ],
+    'images_preview_theme': {
+        'website.s_banner_default_image': '/theme_space/static/src/img/background/img1.jpg',
+        'website.s_cover_default_image': '/theme_space/static/src/img/background/img5.jpg',
+        'website.s_text_image_default_image': '/theme_space/static/src/img/background/img6.jpg',
+        'website.s_picture_default_image': '/theme_space/static/src/img/background/img8.jpg',
+    },
     'assets': {
         'web.assets_frontend': [
             'theme_space/static/fonts/*',
