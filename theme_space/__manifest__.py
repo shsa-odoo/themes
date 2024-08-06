4d18c5d8ff71eb982048dceae37b6d679b7be60f
Sanjay Sharma <shsa@odoo.com>
2024-08-06 14:58:27 +0530

    [Add] theme preview and update configurations for Theme Space
    - Updated `__manifest__.py`:
      - Added comments and new snippet view `s_banner.xml`.
      - Updated image paths.
      - Added configurator snippets.
    - Created new `index.html` in `static/description`.
    - Replaced `space_screenshot.jpg` with `theme_space.jpg`.
    - Added `theme_space.svg`.
    - Updated `primary_variables.scss` to change color palette.
    - Added new snippet template `s_banner.xml`.

--- theme_space/__manifest__.py
+++ theme_space/__manifest__.py
@@ -7,6 +7,7 @@
     'depends': ['website'],
     'data': [
         'data/ir_asset.xml',
+        # i dont think we actually need this
         'data/generate_primary_templates.xml',
         'views/image_library.xml',
 
@@ -16,18 +17,22 @@
         'views/snippets/s_features.xml',
         'views/snippets/s_image_text.xml',
         'views/snippets/s_product_catalog.xml',
+        'views/snippets/s_banner.xml',
         # 'views/footer.xml',
         # 'views/header.xml',
     ],
     'images': [
         'static/description/space_description.jpg',
-        'static/description/space_screenshot.jpg',
+        'static/description/theme_space.jpg',
     ],
     'images_preview_theme': {
         'website.s_banner_default_image': '/theme_space/static/src/img/background/img1.jpg',
         'website.s_cover_default_image': '/theme_space/static/src/img/background/img5.jpg',
         'website.s_text_image_default_image': '/theme_space/static/src/img/background/img6.jpg',
         'website.s_picture_default_image': '/theme_space/static/src/img/background/img8.jpg',
+    },
+    'configurator_snippets': {
+        'homepage': ['s_cover', 's_text_image', 's_title', 's_features', 's_image_text', 's_product_catalog'],
     },
     'assets': {
         'web.assets_frontend': [
