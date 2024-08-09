1499fd11937781b00650259fbed6af2c6a47faee
Sanjay Sharma <shsa@odoo.com>
2024-08-09 17:23:37 +0530

    Final Commit Website-Theme

--- theme_purple/__manifest__.py
+++ theme_purple/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Space Theme',
+    'name': 'Purple Theme',
     'description': 'A custom theme for Odoo',
     'version': '1.0',
     'author': 'Sanjay Sharma',
@@ -8,7 +8,7 @@
     'data': [
         'data/ir_asset.xml',
         # i dont think we actually need this
-        'data/generate_primary_templates.xml',
+        # 'data/generate_primary_templates.xml',
         'views/image_library.xml',
 
         'views/snippets/s_cover.xml',
@@ -22,22 +22,22 @@
         'views/header.xml',
     ],
     'images': [
-        'static/description/space_description.jpg',
-        'static/description/space_screenshot.jpg',
+        'static/description/theme_description.jpg',
+        'static/description/theme_purple.jpg',
     ],
     'images_preview_theme': {
-        'website.s_banner_default_image': '/theme_space/static/src/img/background/img1.jpg',
-        'website.s_cover_default_image': '/theme_space/static/src/img/background/img5.jpg',
-        'website.s_text_image_default_image': '/theme_space/static/src/img/background/img6.jpg',
-        'website.s_picture_default_image': '/theme_space/static/src/img/background/img8.jpg',
+        'website.s_banner_default_image': '/theme_purple/static/src/img/background/img1.jpg',
+        'website.s_cover_default_image': '/theme_purple/static/src/img/background/img5.jpg',
+        'website.s_text_image_default_image': '/theme_purple/static/src/img/background/img6.jpg',
+        'website.s_picture_default_image': '/theme_purple/static/src/img/background/img8.jpg',
     },
     'configurator_snippets': {
         'homepage': ['s_cover', 's_text_image', 's_title', 's_features', 's_image_text', 's_product_catalog', 's_banner'],
     },
     'assets': {
         'web.assets_frontend': [
-            'theme_space/static/fonts/*',
-            'theme_space/static/src/scss/font.scss',
+            'theme_purple/static/fonts/*',
+            'theme_purple/static/src/scss/font.scss',
         ],
     },
     'license': 'LGPL-3',
