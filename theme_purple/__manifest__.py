a5d129d8b2f7041803026f4df542d1f731d3cb05
Sanjay Sharma <shsa@odoo.com>
2024-08-21 22:08:16 +0530

    Lighter code

--- theme_purple/__manifest__.py
+++ theme_purple/__manifest__.py
@@ -6,35 +6,26 @@
     'category': 'Theme/Creative',
     'depends': ['website'],
     'data': [
-        'data/ir_asset.xml',
-        # i dont think we actually need this
-        # 'data/generate_primary_templates.xml',
         'views/image_library.xml',
-
-        'views/snippets/s_cover.xml',
-        'views/snippets/s_text_image.xml',
-        'views/snippets/s_title.xml',
-        'views/snippets/s_features.xml',
-        'views/snippets/s_image_text.xml',
-        'views/snippets/s_product_catalog.xml',
         'views/snippets/s_banner.xml',
-        'views/footer.xml',
-        'views/header.xml',
+        # 'views/footer.xml',
+        # 'views/header.xml',
     ],
+
     'images': [
         'static/description/theme_description.jpg',
         'static/description/theme_purple.jpg',
     ],
     'images_preview_theme': {
         'website.s_banner_default_image': '/theme_purple/static/src/img/background/img1.jpg',
-        'website.s_cover_default_image': '/theme_purple/static/src/img/background/img5.jpg',
-        'website.s_text_image_default_image': '/theme_purple/static/src/img/background/img6.jpg',
-        'website.s_picture_default_image': '/theme_purple/static/src/img/background/img8.jpg',
     },
     'configurator_snippets': {
-        'homepage': ['s_cover', 's_text_image', 's_title', 's_features', 's_image_text', 's_product_catalog', 's_banner'],
+        'homepage': ['s_banner'],
     },
     'assets': {
+        'web._assets_primary_variables': [
+            'theme_purple/static/src/scss/primary_variables.scss',
+        ],
         'web.assets_frontend': [
             'theme_purple/static/fonts/*',
             'theme_purple/static/src/scss/font.scss',
--- theme_purple/models/theme_purple.py
+++ theme_purple/models/theme_purple.py
@@ -4,29 +4,29 @@ class ThemePurple(models.AbstractModel):
 
     _inherit = 'theme.utils'
 
-    # def _theme_purple_post_copy(self, mod):
-    #     # these are available views for headers and footer:
-    #     ##
-    #     # Other examples::
-    #     # website.template_header_default
-    #     # website.template_header_default_align_right
-    #     # website.template_header_hamburger_align_right
-    #     # website.template_header_sales_one
-    #     # website.template_header_sales_three
-    #     # website.template_header_search
-    #     ##
-    #     self.enable_view('website.template_header_hamburger')
+    def _theme_purple_post_copy(self, mod):
+        # these are available views for headers and footer:
+        ##
+        # Other examples::
+        # website.template_header_default
+        # website.template_header_default_align_right
+        # website.template_header_hamburger_align_right
+        # website.template_header_sales_one
+        # website.template_header_sales_three
+        # website.template_header_search
+        ##
+        self.enable_view('website.template_header_search')
 
-    #     ##
-    #     # website.template_footer_headline
-    #     # website.template_footer_contact
-    #     # website.template_footer_descriptive
-    #     # website.template_footer_links
-    #     # website.template_footer_minimalist
-    #     #website.template_footer_slideout
-    #     # #
+        ##
+        # website.template_footer_headline
+        # website.template_footer_contact
+        # website.template_footer_descriptive
+        # website.template_footer_links
+        # website.template_footer_minimalist
+        #website.template_footer_slideout
+        # #
 
-    #     self.enable_view('website.template_footer_descriptive')
+        self.enable_view('website.template_footer_descriptive')
 
 
 
@@ -35,14 +35,14 @@ class ThemePurple(models.AbstractModel):
 
     # example of how to override the header and footer templates to add custom ones
 
-    @property
-    def _header_template(self):
-        return ['theme_purple.custom_header'] + super()._header_template
+    # @property
+    # def _header_template(self):
+    #     return ['theme_purple.custom_header'] + super()._header_template
 
-    @property
-    def _footer_template(self):
-        return ['theme_purple.custom_footer'] + super()._footer_template
+    # @property
+    # def _footer_template(self):
+    #     return ['theme_purple.custom_footer'] + super()._footer_template
 
-    def _theme_purple_post_copy(self, mod):
-        self.enable_view('theme_purple.custom_header')
-        self.enable_view('theme_purple.custom_footer')
+    # def _theme_purple_post_copy(self, mod):
+    #     self.enable_view('theme_purple.custom_header')
+    #     self.enable_view('theme_purple.custom_footer')
